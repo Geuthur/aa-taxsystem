f4a2edafb0f9a9a6fe00d0e401aa297fd24d565e
Geuthur <geuthur@users.noreply.github.com>
2025-02-08 02:22:36 +0100

    [CHANGE] CSS Table [CHANGE] JS Strcture [ADD] Payment System Table

---
+++ taxsystem/static/taxsystem/js/administration.js
@@ -0,0 +1,66 @@
+/* global taxsystemsettings */
+
+$(document).ready(() => {
+    const adminstationTableVar = $('#administration');
+
+    const tableAdministration = adminstationTableVar.DataTable({
+        ajax: {
+            url: taxsystemsettings.corporationadministrationUrl,
+            type: 'GET',
+            dataSrc: function (data) {
+                return Object.values(data[0].corporation);
+            },
+        },
+        columns: [
+            {
+                data: 'character_portrait',
+                render: function (data, _, row) {
+                    return data;
+                }
+            },
+            {
+                data: 'character_name',
+                render: function (data, _, row) {
+                    return data;
+                }
+            },
+            {
+                data: 'status',
+                render: function (data, _, row) {
+                    return data;
+                }
+            },
+            {
+                data: 'actions',
+                render: function (data, _, row) {
+                    return data;
+                }
+            },
+        ],
+        order: [[1, 'asc']],
+        columnDefs: [
+            { orderable: false, targets: [0, 2] },
+        ],
+        filterDropDown: {
+            columns: [
+                {
+                    idx: 2,
+                    maxWidth: '200px',
+                }
+            ],
+            autoSize: false,
+            bootstrap: true,
+            bootstrap_version: 5
+        },
+    });
+
+    tableAdministration.on('init.dt', function () {
+        adminstationTableVar.removeClass('d-none');
+    });
+
+    tableAdministration.on('draw', function (row, data) {
+        $('[data-tooltip-toggle="taxsystem-tooltip"]').tooltip({
+            trigger: 'hover',
+        });
+    });
+});
