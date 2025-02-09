db97633e5c40004c7afa312c88b92cfce4f4833e
Geuthur <geuthur@users.noreply.github.com>
2025-02-09 15:32:56 +0100

    [CHANGE] Update JS Structure [ADD] Editable Hint

--- taxsystem/static/taxsystem/js/manage.js
+++ taxsystem/static/taxsystem/js/manage.js
@@ -1,21 +1,18 @@
 /* global taxsystemsettings */
+$(document).ready(function() {
+    const manageDashboardTableVar = $('#manage-dashboard');
 
-const manageDashboardTableVar = $('#manage-dashboard');
-
-$.ajax({
-    url: taxsystemsettings.corporationmanageDashboardUrl,
-    type: 'GET',
-    success: function (data) {
-        const corporationData = Object.values(data[0].corporation);
-
-        corporationData.forEach(item => {
-            var tax_amount = parseFloat(item.tax_amount);
-            var days = parseFloat(item.tax_period);
-            $('#dashboard-info').html(item.corporation_logo + ' ' + item.corporation_name);
-            $('#last_update_wallet').text(item.last_update_wallet);
-            $('#last_update_members').text(item.last_update_members);
-            $('#last_update_payments').text(item.last_update_payments);
-            $('#last_update_paymentsystem').text(item.last_update_filters);
+    $.ajax({
+        url: taxsystemsettings.corporationmanageDashboardUrl,
+        type: 'GET',
+        success: function (data) {
+            var tax_amount = parseFloat(data.tax_amount);
+            var days = parseFloat(data.tax_period);
+            $('#dashboard-info').html(data.corporation_logo + ' ' + data.corporation_name);
+            $('#last_update_wallet').text(data.last_update_wallet);
+            $('#last_update_members').text(data.last_update_members);
+            $('#last_update_payments').text(data.last_update_payments);
+            $('#last_update_paymentsystem').text(data.last_update_filters);
             $('#taxamount').text(tax_amount);
             $('#period').text(days);
 
@@ -30,7 +27,7 @@ $.ajax({
             // Initialize x-editable
             $('#taxamount').editable({
                 type: 'text',
-                pk: item.id,
+                pk: data.corporation_id,
                 url: updateTaxAmountUrl,
                 title: taxsystemsettings.translations.enterTaxAmount,
                 display: function(value) {
@@ -55,7 +52,7 @@ $.ajax({
 
             $('#period').editable({
                 type: 'text',
-                pk: item.id,
+                pk: data.corporation_id,
                 url: updateTaxPeriodUrl,
                 title: taxsystemsettings.translations.enterTaxPeriod,
                 display: function(value) {
@@ -77,6 +74,7 @@ $.ajax({
                     return response.responseJSON.message;
                 }
             });
+
             $('#taxamount').on('shown', function(e, editable) {
                 // Display tax amount without formatting in the editable field
                 editable.input.$input.val(editable.value.replace(/\./g, '').replace(' ISK', ''));
@@ -86,156 +84,156 @@ $.ajax({
                 // Display tax period without formatting in the editable field
                 editable.input.$input.val(editable.value.replace(' days', ''));
             });
-        });
 
-        manageDashboardTableVar.removeClass('d-none');
-    },
-    error: function(xhr, status, error) {
-        console.error('Error fetching data:', error);
-    }
-});
-
-const membersTableVar = $('#members');
-
-const tableMembers = membersTableVar.DataTable({
-    ajax: {
-        url: taxsystemsettings.corporationMembersUrl,
-        type: 'GET',
-        dataSrc: function (data) {
-            return Object.values(data[0].corporation);
+            manageDashboardTableVar.removeClass('d-none');
         },
-    },
-    columns: [
-        {
-            data: 'character_portrait',
-            render: function (data, _, __) {
-                return data;
-            }
-        },
-        {
-            data: 'character_name',
-            render: function (data, _, __) {
-                return data;
-            }
-        },
-        {
-            data: 'status',
-            render: function (data, _, __) {
-                return data;
-            }
-        },
-        {
-            data: 'joined',
-            render: function (data, _, __) {
-                return data;
-            }
-        },
-    ],
-    order: [[1, 'asc']],
-    columnDefs: [
-        { orderable: false, targets: [0, 2] },
-    ],
-    filterDropDown: {
-        columns: [
-            {
-                idx: 2,
-                maxWidth: '200px',
-            }
-        ],
-        autoSize: false,
-        bootstrap: true,
-        bootstrap_version: 5
-    },
-    rowCallback: function(row, data) {
-        if (data.is_faulty) {
-            $(row).css('background-color', 'rgba(255, 0, 0, 0.1)');
+        error: function(xhr, status, error) {
+            console.error('Error fetching data:', error);
         }
-    },
-});
-
-
-tableMembers.on('draw', function () {
-    $('[data-tooltip-toggle="taxsystem-tooltip"]').tooltip({
-        trigger: 'hover',
     });
-});
 
-tableMembers.on('init.dt', function () {
-    membersTableVar.removeClass('d-none');
-});
+    const membersTableVar = $('#members');
 
-const PaymentSystemTableVar = $('#payment-system');
-
-const tablePaymentSystem = PaymentSystemTableVar.DataTable({
-    ajax: {
-        url: taxsystemsettings.corporationPaymentSystemUrl,
-        type: 'GET',
-        dataSrc: function (data) {
-            return Object.values(data[0].corporation);
+    const tableMembers = membersTableVar.DataTable({
+        ajax: {
+            url: taxsystemsettings.corporationMembersUrl,
+            type: 'GET',
+            dataSrc: function (data) {
+                return Object.values(data[0].corporation);
+            },
         },
-    },
-    columns: [
-        {
-            data: 'character_portrait',
-            render: function (data, _, row) {
-                return data;
-            }
-        },
-        {
-            data: 'character_name',
-            render: function (data, _, row) {
-                return data;
-            }
-        },
-        {
-            data: 'status',
-            render: function (data, _, row) {
-                return data;
-            }
-        },
-        {
-            data: 'wallet',
-            render: function (data, _, row) {
-                const amount = parseFloat(data);
-                return amount.toLocaleString('de-DE') + ' ISK';
-            }
-        },
-        {
-            data: 'has_paid',
-            render: function (data, _, row) {
-                return data;
-            }
-        },
-        {
-            data: 'actions',
-            render: function (data, _, row) {
-                return data;
-            }
-        },
-    ],
-    order: [[1, 'asc']],
-    columnDefs: [
-        { orderable: false, targets: [0, 2] },
-    ],
-    filterDropDown: {
         columns: [
             {
-                idx: 2,
-                maxWidth: '200px',
-            }
+                data: 'character_portrait',
+                render: function (data, _, __) {
+                    return data;
+                }
+            },
+            {
+                data: 'character_name',
+                render: function (data, _, __) {
+                    return data;
+                }
+            },
+            {
+                data: 'status',
+                render: function (data, _, __) {
+                    return data;
+                }
+            },
+            {
+                data: 'joined',
+                render: function (data, _, __) {
+                    return data;
+                }
+            },
         ],
-        autoSize: false,
-        bootstrap: true,
-        bootstrap_version: 5
-    },
-});
+        order: [[1, 'asc']],
+        columnDefs: [
+            { orderable: false, targets: [0, 2] },
+        ],
+        filterDropDown: {
+            columns: [
+                {
+                    idx: 2,
+                    maxWidth: '200px',
+                }
+            ],
+            autoSize: false,
+            bootstrap: true,
+            bootstrap_version: 5
+        },
+        rowCallback: function(row, data) {
+            if (data.is_faulty) {
+                $(row).css('background-color', 'rgba(255, 0, 0, 0.1)');
+            }
+        },
+    });
 
-tablePaymentSystem.on('init.dt', function () {
-    PaymentSystemTableVar.removeClass('d-none');
-});
 
-tablePaymentSystem.on('draw', function (row, data) {
-    $('[data-tooltip-toggle="taxsystem-tooltip"]').tooltip({
-        trigger: 'hover',
+    tableMembers.on('draw', function () {
+        $('[data-tooltip-toggle="taxsystem-tooltip"]').tooltip({
+            trigger: 'hover',
+        });
+    });
+
+    tableMembers.on('init.dt', function () {
+        membersTableVar.removeClass('d-none');
+    });
+
+    const PaymentSystemTableVar = $('#payment-system');
+
+    const tablePaymentSystem = PaymentSystemTableVar.DataTable({
+        ajax: {
+            url: taxsystemsettings.corporationPaymentSystemUrl,
+            type: 'GET',
+            dataSrc: function (data) {
+                return Object.values(data[0].corporation);
+            },
+        },
+        columns: [
+            {
+                data: 'character_portrait',
+                render: function (data, _, row) {
+                    return data;
+                }
+            },
+            {
+                data: 'character_name',
+                render: function (data, _, row) {
+                    return data;
+                }
+            },
+            {
+                data: 'status',
+                render: function (data, _, row) {
+                    return data;
+                }
+            },
+            {
+                data: 'wallet',
+                render: function (data, _, row) {
+                    const amount = parseFloat(data);
+                    return amount.toLocaleString('de-DE') + ' ISK';
+                }
+            },
+            {
+                data: 'has_paid',
+                render: function (data, _, row) {
+                    return data;
+                }
+            },
+            {
+                data: 'actions',
+                render: function (data, _, row) {
+                    return data;
+                }
+            },
+        ],
+        order: [[1, 'asc']],
+        columnDefs: [
+            { orderable: false, targets: [0, 2] },
+        ],
+        filterDropDown: {
+            columns: [
+                {
+                    idx: 2,
+                    maxWidth: '200px',
+                }
+            ],
+            autoSize: false,
+            bootstrap: true,
+            bootstrap_version: 5
+        },
+    });
+
+    tablePaymentSystem.on('init.dt', function () {
+        PaymentSystemTableVar.removeClass('d-none');
+    });
+
+    tablePaymentSystem.on('draw', function (row, data) {
+        $('[data-tooltip-toggle="taxsystem-tooltip"]').tooltip({
+            trigger: 'hover',
+        });
     });
 });
