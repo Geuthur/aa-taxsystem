7a16c436f7671bbb21575c17a2d33c472d8ffbea
Geuthur <geuthur@users.noreply.github.com>
2025-02-09 01:19:17 +0100

    [ADD] Approve Payment View

--- taxsystem/views.py
+++ taxsystem/views.py
@@ -3,14 +3,18 @@
 # Django
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, permission_required
+from django.db import IntegrityError, transaction
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.translation import gettext_lazy as trans
+from django.views.decorators.http import require_POST
 from esi.decorators import token_required
 
 from allianceauth.eveonline.models import EveCharacter, EveCorporationInfo
 
+from taxsystem.api.helpers import get_corporation
 from taxsystem.helpers.views import add_info_to_context
 from taxsystem.models import OwnerAudit
+from taxsystem.models.tax import Payments, PaymentSystem
 from taxsystem.tasks import update_corp
 
 from .hooks import get_extension_logger
@@ -105,3 +109,46 @@ def overview(request):
     context = add_info_to_context(request, context)
 
     return render(request, "taxsystem/admin/overview.html", context=context)
+
+
+@login_required
+@permission_required("taxsystem.manage_access")
+@require_POST
+def approve_payment(request, corporation_id: int, payment_pk: int):
+    # Check Permission
+    perms, corp = get_corporation(request, corporation_id)
+    previous_url = request.headers.get("referer", "taxsystem:payments")
+
+    if not perms:
+        msg = trans("Permission Denied")
+        messages.error(request, msg)
+        return redirect(previous_url)
+
+    try:
+        with transaction.atomic():
+            payment = Payments.objects.get(
+                payment_user__corporation=corp, pk=payment_pk
+            )
+            if payment.payment_status in [
+                Payments.States.PENDING,
+                Payments.States.NEEDS_APPROVAL,
+            ]:
+                payment.approved = Payments.Approval.APPROVED
+                payment.payment_status = Payments.States.PAID
+                payment.system = Payments.Systems.MANUAL
+                payment.save()
+
+                payment_user = PaymentSystem.objects.get(
+                    corporation=corp, user=payment.payment_user.user
+                )
+                payment_user.payment_pool += payment.amount
+                payment_user.save()
+
+                msg = trans("Payment ID: %s successfully approved") % payment.pk
+            else:
+                msg = trans("Payment ID: %s is already edited") % payment.pk
+    except IntegrityError:
+        msg = trans("Transaction failed. Please try again.")
+
+    messages.info(request, msg)
+    return redirect(previous_url)
