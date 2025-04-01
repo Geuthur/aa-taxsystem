10bf4e09e3f5febd863d035ecc9d508396dded10
Geuthur <geuthur@users.noreply.github.com>
2025-04-01 21:41:50 +0200

    [CHANGE] Optimize Code

--- taxsystem/api/taxsystem/helpers/administration.py
+++ taxsystem/api/taxsystem/helpers/administration.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from django.utils.html import format_html
 from django.utils.translation import gettext as _
 
-from taxsystem.api.helpers import generate_button, generate_settings
+from taxsystem.api.helpers import generate_button
 from taxsystem.models.tax import Members
 
 
@@ -12,36 +12,32 @@ def _delete_member(corporation_id, member: Members, perms, request):
         return ""
 
     template = "taxsystem/partials/form/button.html"
-    confirm_text = ""
-    confirm_text += _("Are you sure to Confirm")
     url = reverse(
         viewname="taxsystem:delete_user",
         kwargs={"corporation_id": corporation_id, "member_pk": member.pk},
     )
 
     if member.is_missing:
-        confirm_text += (
-            f"?<br><span class='fw-bold'>{member.character_name} "
+        confirm_text = (
+            _("Are you sure to Confirm")
+            + f"?<br><span class='fw-bold'>{member.character_name} "
             + _("Delete")
             + "</span>"
         )
-        title = _("Delete Member")
-        icon = "fas fa-trash"
-        color = "danger"
 
-        settings = generate_settings(
-            title=title,
-            icon=icon,
-            color=color,
-            text=confirm_text,
-            modal="members-delete-member",
-            action=url,
-            ajax="action",
-        )
+        settings_data = {
+            "title": _("Delete Member"),
+            "icon": "fas fa-trash",
+            "color": "danger",
+            "text": confirm_text,
+            "modal": "members-delete-member",
+            "action": url,
+            "ajax": "action",
+        }
         # Generate the buttons
         actions = []
         actions.append(
-            generate_button(corporation_id, template, member, settings, request)
+            generate_button(corporation_id, template, member, settings_data, request)
         )
 
         actions_html = format_html("".join(actions))
