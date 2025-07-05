849cb398e6ef66f2d2c9ac74b5a7cd255c4cf992
Geuthur <761682+Geuthur@users.noreply.github.com>
2025-07-05 13:24:23 +0200

    [FIX] Missing static img (#39)

--- taxsystem/__init__.py
+++ taxsystem/__init__.py
@@ -3,7 +3,7 @@
 # AA TaxSystem
 from taxsystem.app_settings import TAXSYSTEM_APP_NAME
 
-__version__ = "0.5.7"
+__version__ = "0.5.8"
 __title__ = "Tax System"
 
 USER_AGENT_TEXT = f"{TAXSYSTEM_APP_NAME} v{__version__}"
